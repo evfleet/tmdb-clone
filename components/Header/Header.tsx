1a8dc3247abe384712f79d8c6a06b325df3f3286
Evan Fleet <evfleet@gmail.com>
2018-06-02 19:43:46 -0400

    Rename server folder

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,24 @@ import * as React from "react";
 
 import styles from "./Header.scss";
 
-const Header = () => <header className={styles.text}>Header</header>;
+import Navigation from "./components/Navigation";
+import SearchBar from "./components/SearchBar";
+
+const Header = () => (
+  <header>
+    <div className={styles["navigation-content"]}>
+      <div>
+        <img
+          className={styles.logo}
+          alt="Logo for The Movie Database (TMDb)"
+          src="https://www.themoviedb.org/static_cache/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg"
+        />
+      </div>
+      <Navigation />
+    </div>
+
+    <SearchBar />
+  </header>
+);
 
 export default Header;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ class CustomDocument extends Document {
     return (
       <html>
         <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
+          />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
